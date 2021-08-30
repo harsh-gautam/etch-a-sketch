a2b2d7044a3636daefd512119ffed2f252ce266f
Harsh Gautam <hgautam208@gmail.com>
2021-08-30 22:27:14 +0530

    creating a grid of nxn dynamically

--- js/index.js
+++ js/index.js
@@ -1,6 +1,24 @@
 const sliderValue = document.querySelector('.slider-value')
 const slider = document.querySelector('#grid-size-slider')
+sliderValue.innerText = slider.value;
 
 slider.addEventListener('input', (e) => {
   sliderValue.innerText = e.target.value;
+  createGrid(parseInt(e.target.value))
 })
+
+function createGrid(gridSize) {
+  const canvas = document.querySelector(".canvas")
+  canvas.innerHTML = "";
+  for(let i = 0; i < gridSize; i++){
+    let rowDiv = document.createElement('div')
+    rowDiv.classList.add("canvas-row")
+    for(let j = 0; j< gridSize; j++){
+      let div = document.createElement("div")
+      div.className = ["grid-cell"];
+      div.style.backgroundColor = "rgba(120, 0 ,0, 0.4 )";
+      rowDiv.appendChild(div);
+    }
+    canvas.appendChild(rowDiv)
+  }
+}
